96456e469592cdfeb9b71a996b50f82856bb75b6
tokamgamal <toka.mgamal@gmail.com>
2025-12-02 11:11:14 +0200

    Appium - Course Part 1 [Basics]

--- src/test/java/Appium/appiumbasics.java
+++ src/test/java/Appium/appiumbasics.java
@@ -1,6 +1,8 @@
 package Appium;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
@@ -8,6 +10,7 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -20,9 +23,11 @@ public class appiumbasics extends BaseTest{
     
     @Test
     public void appiumTest() throws MalformedURLException, URISyntaxException {
+        Activity activity = new Activity("io.appium.android.apis","io.appium.android.apis.preference.PreferenceDependencies");
+        ((JavascriptExecutor)driver).executeScript("mobile: startActivity", ImmutableMap.of("intent","io.appium.android.apis/io.appium.android.apis.preference.PreferenceDependencies"));
 
-        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
+//        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+//        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
         driver.findElement(AppiumBy.id("android:id/checkbox")).click();
         DeviceRotation landscape = new DeviceRotation(0,0,90);
         driver.rotate(landscape);
